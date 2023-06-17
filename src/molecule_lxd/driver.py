bff8bfbeabf04636084839e685280fc7c74eda8b
Bernhard Strähle <bernhard.straehle@gmail.com>
2023-06-17 17:48:46 +0200

    Cleanup docstrings in driver.py

--- src/molecule_lxd/driver.py
+++ src/molecule_lxd/driver.py
@@ -22,67 +22,11 @@ from typing import Dict
 from molecule import logger
 from molecule.api import Driver
 
-
 LOG = logger.get_logger(__name__)
 
 
 class LXD(Driver):
-    """
-    The class responsible for managing `LXD`_ containers.  `LXD`_ is `not` the
-    default driver used in Molecule.
-
-    Molecule leverages Ansible's `lxd_container`_ module, by mapping variables
-    from ``molecule.yml`` into ``create.yml`` and ``destroy.yml``.
-    The `lxd_container`_ module leverages the LXD API. Useful information
-    about, for example the source variable can be found in the `LXD API documentation`.
-
-    .. _`lxd_container`: https://docs.ansible.com/ansible/latest/lxd_container_module.html
-    .. _`LXD API documentation`: https://github.com/lxc/lxd/blob/master/doc/rest-api.md#post-1
-
-    .. code-block:: yaml
-
-        driver:
-          name: lxd
-        platforms:
-          - name: instance
-            url: https://127.0.0.1:8443
-            snap_url: unix:/var/snap/lxd/common/lxd/unix.socket
-            cert_file: /root/.config/lxc/client.crt
-            key_file: /root/.config/lxc/client.key
-            trust_password: password
-            source:
-              type: image
-              mode: pull
-              server: https://images.linuxcontainers.org
-              protocol: lxd|simplestreams
-              alias: ubuntu/jammy/amd64
-            architecture: x86_64|i686
-            config:
-              limits.cpu: 2
-            devices:
-              kvm:
-                path: /dev/kvm
-                type: unix-char
-            profiles:
-              - default
-            force_stop: true
-            ignore_volatile_options: false
-            target: node_name
-            type: virtual-machine
-            install_basic_packages: false
-
-    Provide a list of files Molecule will preserve, relative to the scenario
-    ephemeral directory, after any ``destroy`` subcommand execution.
-
-    .. code-block:: yaml
-
-        driver:
-          name: lxd
-          safe_files:
-            - foo
-
-    .. _`LXD`: https://linuxcontainers.org/lxd/introduction/
-    """  # noqa
+    """LXD molecule driver plugin."""
 
     def __init__(self, config=None):
         super(LXD, self).__init__(config)
@@ -118,13 +62,10 @@ class LXD(Driver):
         }
 
     def sanity_checks(self):
-        # FIXME(decentralize): Implement sanity checks
+        # TODO: Implement sanity checks
         pass
 
     def template_dir(self):
-        """Return path to its own cookiecutter templates. It is used by init
-        command in order to figure out where to load the templates from.
-        """
         return os.path.join(os.path.dirname(__file__), "cookiecutter")
 
     def modules_dir(self):
@@ -132,5 +73,4 @@ class LXD(Driver):
 
     @property
     def required_collections(self) -> Dict[str, str]:
-        """Return collections dict containing names and versions required."""
         return {"community.general": "4.1.0"}
